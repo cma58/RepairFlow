ab84c8b0a3770b07046a99beea25385e56326e86
cma58 <amine.chtaiti@gmail.com>
2026-09-07 16:01:56 +0200

    Load RepairFlow V3.2 analysis fee UI hotfix

--- app-v07.js
+++ app-v07.js
@@ -1,4 +1,10 @@
 'use strict';
+const feeKey='rfAnalysisFeeV31';
+const feeRaw=localStorage.getItem(feeKey);
+const feeValue=Number(feeRaw);
+if(feeRaw===null || !Number.isFinite(feeValue) || feeValue<=0){
+  localStorage.setItem(feeKey,'35');
+}
 const repairFlowV3=document.createElement('script');
 repairFlowV3.src='/app-v3.js';
 repairFlowV3.defer=true;
@@ -6,6 +12,12 @@ repairFlowV3.onload=()=>{
   const analysisFeeLayer=document.createElement('script');
   analysisFeeLayer.src='/app-analysisfee.js';
   analysisFeeLayer.defer=true;
+  analysisFeeLayer.onload=()=>{
+    const analysisFeeUiFix=document.createElement('script');
+    analysisFeeUiFix.src='/app-analysisfee-ui-fix.js';
+    analysisFeeUiFix.defer=true;
+    document.head.appendChild(analysisFeeUiFix);
+  };
   document.head.appendChild(analysisFeeLayer);
 };
 document.head.appendChild(repairFlowV3);
