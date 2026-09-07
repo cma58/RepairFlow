fc84331146df0daf4ba60ee25d849910747a7d20
cma58 <amine.chtaiti@gmail.com>
2026-09-07 15:54:16 +0200

    Load mandatory analysis fee layer after Reparaties V3

--- app-v07.js
+++ app-v07.js
@@ -2,4 +2,10 @@
 const repairFlowV3=document.createElement('script');
 repairFlowV3.src='/app-v3.js';
 repairFlowV3.defer=true;
+repairFlowV3.onload=()=>{
+  const analysisFeeLayer=document.createElement('script');
+  analysisFeeLayer.src='/app-analysisfee.js';
+  analysisFeeLayer.defer=true;
+  document.head.appendChild(analysisFeeLayer);
+};
 document.head.appendChild(repairFlowV3);
